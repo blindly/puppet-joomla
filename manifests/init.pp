1dc809343e6a9b6808fba3b85bfc47d96a9475f0
blindly <blindly@users.noreply.github.com>
2017-09-15 15:51:06 -0400

    Restoring init.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,14 +3,7 @@ class joomla(
                 $db_user='joomla',
                 $db_password='password'
                 )
-{  
-  class {'joomla::app':
-  
-  }
-  
-  class { 'joomla::db':
-    db_name => $db_name,
-    db_user => $db_user,
-    db_password => $db_password,
-  }
+{
+  include joomla::app
+  include joomla::db
 }
